9d5a291d86069c07c738072da63b00da0ac249ca
Vidhula3 <88660571+Vidhula3@users.noreply.github.com>
2022-01-15 12:55:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,13 +4,15 @@ const Bodies = Matter.Bodies;
 
 var engine,world;
 var bg;
-var snow,snow4;
-var snowfall_img;
+var snow,snow1,snow2,snow3;
+var snowfall_img,snowfall1_img
 
 function preload() {
  bg=loadImage("snow3.jpg")
  snowfall_img=loadImage("snow4.webp");
-
+ snowfall1_img=loadImage("snow5.webp");
+ snowfall2_img=loadImage("snow6.png");
+ snowfall3_img=loadImage("snow7.png");
 }
 
 function setup() {
@@ -18,12 +20,41 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  snow=new Snow(300,100,10,10)
 }
 
 function draw() {
   background(bg); 
  Engine.update(engine);
 
-  snow.display();
+ if (frameCount % 80 === 0) {
+  snow = createSprite(100,15);
+  snow.addImage("snow",snowfall_img);
+  snow.scale=0.1;
+  snow.velocityY=4;
+  }
+
+  if (frameCount % 100 === 0) {    
+  snow1 = createSprite(300,60);
+  snow1.addImage("snow1",snowfall1_img);
+  snow1.scale=0.07;
+  snow1.velocityY=4; 
+  var rand = Math.round(random(1,5));
+}
+
+if (frameCount % 150 === 0) {
+  snow2 = createSprite(600,30);
+  snow2.addImage("snow2",snowfall2_img);
+  snow2.scale=0.1;
+  snow2.velocityY=4;
+  }
+
+  if (frameCount % 200 === 0) {
+    snow3 = createSprite(750,90);
+    snow3.addImage("snow3",snowfall3_img);
+    snow3.scale=0.05;
+    snow3.velocityY=4;
+    var rand = Math.round(random(1,5));
+    }
+
+ drawSprites();
 }
